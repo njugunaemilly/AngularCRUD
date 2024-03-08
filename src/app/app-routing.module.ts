9b07b38c1f671667881902482845838510fd3a61
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-08 11:19:01 +0300

    Create component for creating new users and routing, install bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create-employee', component:CreateEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
