b2f5e149ac7960bd3795124d0e230e50d9ed8ff2
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-08 11:52:56 +0300

    Create employee card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employee-details', component:EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
