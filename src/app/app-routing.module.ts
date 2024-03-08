0578aabcfa836cf233e45d531a1fd7c35cca9c1e
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-08 10:58:31 +0300

    Create employee-list component and routing, install bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'view-employees', component:EmployeeListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
