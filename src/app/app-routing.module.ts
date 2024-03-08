e4e05c03c75939cfd16ed33a5b20a60bb022a18a
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-08 11:34:58 +0300

    Create update-employee component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UpdateEmployeesComponent } from './update-employees/update-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'update-employee', component:UpdateEmployeesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
