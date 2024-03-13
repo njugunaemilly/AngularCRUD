24d1b4d2926765b40beba459e6e66a6ca55c3d40
Wanjiku Njuguna <117892624+njugunaemilly@users.noreply.github.com>
2024-03-13 08:43:31 +0300

    Update employee list functionality

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent {
+  employee!: Employee 
 
+  constructor(private employeeService: EmployeeService, private route: Router){}
+
+  saveEmployee(){
+    this.employeeService.addEmployee(this.employee).subscribe(data=>{
+      console.log(data);
+      this.gotToEmployeeList()
+    })
+  }
+
+  gotToEmployeeList(){
+    this.route.navigate(['/view-employees']);
+  }
+
+  onSubmit(){
+    console.log(this.employee);
+    this.saveEmployee();
+  }
 }
